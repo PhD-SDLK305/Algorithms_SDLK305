80b86872acacc6c22ede0503a793737424ec956c
SDLK305 <116006961+PhD-SDLK305@users.noreply.github.com>
2025-12-16 22:54:09 +0700

    FIX array

--- javascript/array/easy/SearchInsertPosition.js
+++ javascript/array/easy/SearchInsertPosition.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    if (target > nums[nums.length - 1]) return nums.length
-    if (target < nums[0] || target == nums[0]) return 0
     let left = 0
-    let right = nums.length
+    let right = nums.length - 1
+
     while (left <= right) {
-        let middle = Math.floor((left + right) / 2)
-        if (nums[middle] == target) return middle
-        if (nums[middle] < target) {
-            left = middle + 1
-        }else right = middle - 1
+        let mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target) return mid
+        if (nums[mid] < target) left = mid + 1
+        else right = mid - 1
     }
-    return right == left ? left + 1 : right + 1
+
+    return left
 };
+
 
 console.log(searchInsert([1,2,5], 3))
